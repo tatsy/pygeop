55fa2da4985ea3483f9ac566d0d7a7cb3eb58f59
tatsy <tatsy.mail@gmail.com>
2017-01-06 17:11:09 +0900

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+class Installer(install):
+    def run(self):
+        super(Installer, self).run(self)
+
+setup(
+    cmdclass={ 'install' : Installer },
+    name='pygeop',
+    version='0.1.0',
+    author='tatsy',
+    author_email='tatsy.mail@gmail.com',
+    url='https://github.com/tatsy/hydra.git',
+    description='Geometry processing library for Python',
+    license='MIT',
+    classfiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
+    package=find_packages()
+)
